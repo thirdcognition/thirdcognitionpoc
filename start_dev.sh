503de9f7c596c6ce9442a7aa1dd4b338d18e8429
mathatan <mathatan@gmail.com>
2024-10-11 17:09:34 +0300

    Create start_dev.sh

---
+++ start_dev.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if we are in a Docker container
+if [ -f /.dockerenv ]; then
+    echo "Running inside Docker. Skipping venv creation."
+else
+    # python3 -m venv .venv
+    echo "Not running inside Docker. Creating virtual environment..."
+    source .venv/bin/activate
+fi
+
+# Start the first process
+streamlit run poctimeline/timeline/timeline.py --server.port 5500 --server.headless true --server.address=0.0.0.0 --client.showErrorDetails=False --client.toolbarMode=minimal --server.fileWatcherType=none &
+
+# Start the second process
+streamlit run poctimeline/admin/Admin_Home.py --server.port 5000 --server.headless true --server.address=0.0.0.0 --client.showErrorDetails=False --client.toolbarMode=minimal --server.fileWatcherType=none &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
